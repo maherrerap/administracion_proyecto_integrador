5f3d952246269442d1394180fc5b1b374daf4e18
martinhrapco <martinhrapco@gmail.com>
2025-12-26 16:15:31 -0500

    Commit #1

---
+++ src/administracion_proyecto_integrador/Administracion_proyecto_integrador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package administracion_proyecto_integrador;
+
+/**
+ *
+ * @author MARTIN
+ */
+public class Administracion_proyecto_integrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
