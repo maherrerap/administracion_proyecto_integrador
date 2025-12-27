f0e3d134630bc7365d209c7863df5fc8f284dba6
martinhrapco <martinhrapco@gmail.com>
2025-12-27 16:06:14 -0500

    DP y MD de Pro_X_Fac

--- src/administracion_proyecto_integrador/dp/Facturacion/Pro_x_Fac.java
+++ src/administracion_proyecto_integrador/dp/Facturacion/Pro_x_Fac.java
@@ -1,13 +1,227 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package administracion_proyecto_integrador.dp.Facturacion;
 
-/**
- *
- * @author MARTIN
- */
+
+import java.util.List;
+import java.util.ArrayList;
+
+import administracion_proyecto_integrador.md.Facturacion.Pro_x_FacMD;
+
 public class Pro_x_Fac {
+    
+    // Atributos de la clase Pro_x_Fac
+    
+    private String idFactura;
+    private String idProducto;
+    private int pxfCantidad;
+    private double pxfPrecio;
+    private double pxfSubtotal;
+    private String estadoPxf;
+    
+    
+    // Constructores
+    
+    // Constructor Vacio
+    public Pro_x_Fac () {}
+    
+    // Constructor completo
+
+    public Pro_x_Fac(String idFactura, String idProducto, int pxfCantidad, double pxfPrecio, double pxfSubtotal, String estadoPxf) {
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.pxfCantidad = pxfCantidad;
+        this.pxfPrecio = pxfPrecio;
+        this.pxfSubtotal = pxfSubtotal;
+        this.estadoPxf = estadoPxf;
+    }
+    
+    // Getters
+
+    public String getIdFactura() {
+        return idFactura;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public int getPxfCantidad() {
+        return pxfCantidad;
+    }
+
+    public double getPxfPrecio() {
+        return pxfPrecio;
+    }
+
+    public double getPxfSubtotal() {
+        return pxfSubtotal;
+    }
+
+    public String getEstadoPxf() {
+        return estadoPxf;
+    }
+    
+    // Setters
+
+    public void setIdFactura(String idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setPxfCantidad(int pxfCantidad) {
+        this.pxfCantidad = pxfCantidad;
+    }
+
+    public void setPxfPrecio(double pxfPrecio) {
+        this.pxfPrecio = pxfPrecio;
+    }
+
+    public void setPxfSubtotal(double pxfSubtotal) {
+        this.pxfSubtotal = pxfSubtotal;
+    }
+
+    public void setEstadoPxf(String estadoPxf) {
+        this.estadoPxf = estadoPxf;
+    }
+    
+    
+    // Métodos para reglas de negocio:
+    
+    // 1. Calcular el Subtotal
+    
+    public static double calcularSubtotal (int cantidad, double precio) {
+        return cantidad * precio;
+    }
+    
+    public void recalcularSubtotal() {
+        this.pxfSubtotal = calcularSubtotal(this.pxfCantidad, this.pxfPrecio);
+    }
+    
+    
+    // 2. Validar Detalle
+    
+    public static void validarDetalle(String idFactura, String idProducto, int cantidad) {
+        if (idFactura == null || idFactura.trim().isEmpty())
+            throw new IllegalArgumentException("El identificador es obligatorio.");
+        if(idProducto == null || idProducto.trim().isEmpty())
+            throw new IllegalArgumentException("El identificador es obligatorio.");
+        if (cantidad <= 0)
+            throw new IllegalArgumentException("La cantidad debe ser mayor a 0.");
+    }
+    
+    // Metodos de conexion con la BD
+    
+    /**
+     * Cargar tabla: Detalle de productos de una factura
+     */
+    
+    public static List<Pro_x_Fac> obtenerDetallesFactura(String idFactura) throws Exception {
+        return Pro_x_FacMD.obtenerListadoDetallesFactura(idFactura);
+    }
+    
+    /**
+     * Buscar un producto especifico en el detalle
+     */
+    
+    public static Pro_x_Fac obtenerDetalle(String idFactura, String idProducto) throws Exception {
+        validarDetalle(idFactura, idProducto, 1);
+        return Pro_x_FacMD.obtenerDetalle(idFactura, idProducto);
+    }
+    
+    /**
+     * Registrar Producto en el detalle
+     * Regla a tomar en cuenta: Si ya existe el producto en la factura, 
+     * se incrementa la cantidad
+     */
+    
+    public static boolean agregarProducto(String idFactura, String idProducto, int cantidad) throws Exception {
+        validarDetalle(idFactura, idProducto, cantidad);
+        
+        Pro_x_Fac existente = obtenerDetalle(idFactura, idProducto);
+        
+        if (existente == null) {
+            double precioActual = Pro_x_FacMD.obtenerPrecioVentaProducto(idProducto);
+            
+            Pro_x_Fac nuevo = new Pro_x_Fac();
+            nuevo.setIdFactura(idFactura);
+            nuevo.setIdProducto(idProducto);
+            nuevo.setPxfCantidad(cantidad);
+            nuevo.setPxfPrecio(precioActual);
+            nuevo.recalcularSubtotal();
+            nuevo.setEstadoPxf("APR");  // Por regla de negocio se generan facturas en estado aprobado
+            
+            return Pro_x_FacMD.registrarDetalle(nuevo);
+        } else {
+            int nuevaCantidad = existente.getPxfCantidad() + cantidad;
+            return actualizarCantidad(idFactura, idProducto, nuevaCantidad);
+        }
+    }
+    
+    /**
+     * Aumentar cantidad de producto en el detalle con el boton "+"
+     */
+    
+    public static boolean incrementarCantidad(String idFactura, String idProducto, int paso) throws Exception {
+        if (paso <= 0)
+            paso = 1;
+        
+        Pro_x_Fac detalle = obtenerDetalle(idFactura, idProducto);
+        
+        if (detalle == null)
+            throw new IllegalArgumentException("No existe el producto en el detalle.");
+        
+        int nuevaCantidad= detalle.getPxfCantidad() + paso;
+        return actualizarCantidad (idFactura, idProducto, nuevaCantidad);
+    }
+    
+    /**
+     * Disminuir cantidad de producto en el detalle con el boton "-"
+     */
+    
+    public static boolean decrementarCantidad(String idFactura, String idProducto, int paso) throws Exception {
+        if (paso <= 0)
+            paso = 1;
+        
+        Pro_x_Fac detalle = obtenerDetalle(idFactura, idProducto);
+        
+        if (detalle == null)
+            throw new IllegalArgumentException("No existe el producto en el detalle.");
+        
+        int nuevaCantidad= detalle.getPxfCantidad() - paso;
+        
+        
+        if (nuevaCantidad <= 0) {
+            return eliminarDetalle(idFactura, idProducto);
+        }
+        
+        return actualizarCantidad(idFactura, idProducto, nuevaCantidad);
+    }
+
+    /**
+     * Actualizar la cantidad y recalcular el subtotal del producto
+     */
+    
+    public static boolean actualizarCantidad(String idFactura, String idProducto, int nuevaCantidad) throws Exception {
+        validarDetalle(idFactura, idProducto, nuevaCantidad);
+        
+        double precio = Pro_x_FacMD.obtenerPrecioDetalle(idFactura, idProducto);
+        double nuevoSubtotal = calcularSubtotal(nuevaCantidad, precio);
+        
+        return Pro_x_FacMD.actualizarCantidadYSubtotal(idFactura, idProducto, nuevaCantidad, nuevoSubtotal);
+    }
+    
+    /**
+     * Eliminar Producto de Detalle
+     * Como se trata del detalle, aqui si se realiza un borrado fisico del mismo.
+     */
+    public static boolean eliminarDetalle(String idFactura, String idProducto) throws Exception {
+        validarDetalle(idFactura, idProducto, 1);
+        return Pro_x_FacMD.eliminarDetalle(idFactura, idProducto);
+    }
+    
+    
     
 }
--- src/administracion_proyecto_integrador/md/Facturacion/Pro_x_FacMD.java
+++ src/administracion_proyecto_integrador/md/Facturacion/Pro_x_FacMD.java
@@ -1,13 +1,273 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package administracion_proyecto_integrador.md.Facturacion;
 
-/**
- *
- * @author MARTIN
- */
+import administracion_proyecto_integrador.dp.Facturacion.Pro_x_Fac;
+import administracion_proyecto_integrador.conexion.ConexionPostgreSQL;
+
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pro_x_FacMD {
     
+    private static Pro_x_Fac mapearDetalle (ResultSet rs) throws SQLException {
+        Pro_x_Fac detalle = new Pro_x_Fac();
+        
+        detalle.setIdFactura(rs.getString("id_factura"));
+        detalle.setIdProducto(rs.getString("id_producto"));
+        detalle.setPxfCantidad(rs.getInt("pxf_cantidad"));
+        detalle.setPxfPrecio(rs.getDouble("pxf_precio"));
+        detalle.setPxfSubtotal(rs.getDouble("pxf_subtotal"));
+        detalle.setEstadoPxf(rs.getString("estado_pxf"));
+        
+        return detalle;
+    }
+    
+    
+    // CONSULTA DETALLES POR FACTURA
+    
+    public static List<Pro_x_Fac> obtenerListadoDetallesFactura(String idFactura) {
+        List<Pro_x_Fac> lista = new ArrayList<>();
+        
+        String sql = "SELECT * FROM pro_x_fac WHERE id_factura = ? AND estado_pxf = 'APR' ORDER BY id_producto";
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, idFactura);
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    lista.add(mapearDetalle(rs));
+                }
+            }
+        } catch(SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+        }
+        
+        return lista;
+    }
+    
+    // CONSULTA ESPECIFICA DE DETALLE
+    
+    public static Pro_x_Fac obtenerDetalle(String idFactura, String idProducto) {
+        String sql =
+            "SELECT * FROM pro_x_fac " +
+            "WHERE id_factura = ? AND id_producto = ?";
+
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, idFactura);
+            ps.setString(2, idProducto);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return mapearDetalle(rs);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+        }
+        return null;
+    }
+    
+    // OBTENER PRECIO DE VENTA DEL PRODUCTO
+    
+    public static double obtenerPrecioVentaProducto(String idProducto) {
+        String sql = "SELECT pro_precio_venta FROM productos WHERE id_producto = ? AND estado_prod = 'ACT'";
+
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, idProducto);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getDouble(1);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+        }
+        return 0.0;
+    }
+    
+    
+    // OBTENER EL PRECIO DEL DETALLE
+    
+    public static double obtenerPrecioDetalle(String idFactura, String idProducto) {
+        String sql =
+            "SELECT pxf_precio " +
+            "FROM pro_x_fac " +
+            "WHERE id_factura = ? AND id_producto = ?";
+
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, idFactura);
+            ps.setString(2, idProducto);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getDouble(1);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+        }
+
+        return 0.0;
+    }
+    
+    // ACTUALIZAR LA CANTIDAD Y EL SUBTOTAL
+    
+    public static boolean actualizarCantidadYSubtotal(String idFactura, String idProducto, int nuevaCantidad, double nuevoSubtotal) {
+        String sql =
+            "UPDATE pro_x_fac SET " +
+            "pxf_cantidad = ?, " +
+            "pxf_subtotal = ? " +
+            "WHERE id_factura = ? AND id_producto = ?";
+
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, nuevaCantidad);
+            ps.setDouble(2, nuevoSubtotal);
+            ps.setString(3, idFactura);
+            ps.setString(4, idProducto);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+            return false;
+        }
+    }
+    
+    // VERIFICAR EXISTENCIA
+    
+    public static boolean verificarExistencia(String idFactura, String idProducto) {
+        String sql = "SELECT 1 FROM pro_x_fac WHERE id_factura =  ? AND id_producto = ? AND estado_pxf = 'APR'";
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, idFactura);
+            ps.setString(2, idProducto);
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+            return false;
+        }
+    }
+    
+    
+    // OBTENER CANTIDAD ACTUAL DE PRODUCTO
+    
+    public static int obtenerCantidadActual(String idFactura, String idProducto) {
+        String sql = "SELECT pxf_cantidad FROM pro_x_fac WHERE id_factura = ? AND id_producto = ? AND estado_pxf = 'APR'";
+        
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, idFactura);
+            ps.setString(2, idProducto);
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next())
+                    return rs.getInt("pxf_cantidad");
+            }
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+        }
+        
+        return 0;
+    }
+    
+    // CREAR DETALLE
+    
+    public static boolean registrarDetalle(Pro_x_Fac detalle) {
+        String sql = "INSERT INTO pro_x_fac (id_factura, id_producto, "
+                + "pxf_cantidad, pxf_precio, pxf_subtotal, estado_pxf) "
+                + "VALUES (?,?,?,?,?,?)";
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, detalle.getIdFactura());
+            ps.setString(2, detalle.getIdProducto());
+            ps.setInt(3, detalle.getPxfCantidad());
+            ps.setDouble(4, detalle.getPxfPrecio());
+            ps.setDouble(5, detalle.getPxfSubtotal());
+            ps.setString(6, detalle.getEstadoPxf());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+            return false;
+        }
+    }
+    
+    // ACTUALIZAR LA CANTIDAD CON LOS BOTONES DE '+' O '-' Y RECALCULAR SUBTOTAL
+    
+    public boolean actualizarCantidad(String idFactura, String idProducto, int nuevaCantidad, double precioUnitario) {
+        String sql = "UPDATE pro_x_fac SET pxf_cantidad = ?, pxf_subtotal = ? "
+                + "WHERE id_factura = ? AND id_producto = ? AND estado_pxf = 'APR'";
+        
+        double nuevoSubtotal = nuevaCantidad * precioUnitario;
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, nuevaCantidad);
+            ps.setDouble(2, nuevoSubtotal);
+            ps.setString(3, idFactura);
+            ps.setString(4, idProducto);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+            return false;
+        }
+    }
+    
+    // Sumar Cantidad (si ya existe el producto)
+    
+    public boolean sumarCantidad(String idFactura, String idProducto, int cantidad, double precioUnitario) {
+        int actual = obtenerCantidadActual(idFactura, idProducto);
+        int nueva = actual + cantidad;
+        return actualizarCantidad(idFactura, idProducto, nueva, precioUnitario);
+    }
+    
+    // Eliminar Producto de Detalle
+    
+    public static boolean eliminarDetalle(String idFactura, String idProducto) {
+        String sql = "DELETE FROM pro_x_fac WHERE id_factura = ? "
+                + "AND id_producto = ? AND estado_pxf = 'APR'";
+        
+        try (Connection conn = ConexionPostgreSQL.getConnection();
+                PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, idFactura);
+            ps.setString(2, idProducto);
+            
+            return ps.executeUpdate() > 0;
+        } catch(SQLException e) {
+            System.out.println("No se pudo completar la operación. Intente de nuevo.");
+            return false;
+        }
+    }
 }
